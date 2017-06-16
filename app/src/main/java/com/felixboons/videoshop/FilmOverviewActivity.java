9f06d698ea3bb18e149fbd664f1e5310e4581d3b
jesse <jwit11@avans.nl>
2017-06-16 21:33:46 +0200

    Fixed toolbar

--- app/src/main/java/com/felixboons/videoshop/FilmOverviewActivity.java
+++ app/src/main/java/com/felixboons/videoshop/FilmOverviewActivity.java
@@ -2,6 +2,7 @@ package com.felixboons.videoshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class FilmOverviewActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class FilmOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_overview);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myToolbar.setTitle("Film Overview");
+        setSupportActionBar(myToolbar);
     }
 }
